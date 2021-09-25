f8abce187e2777d1719d6ce532329ee4274601ca
김민우 <69840777+Kim-Minu@users.noreply.github.com>
2021-09-25 13:52:32 +0900

    2장

---
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -0,0 +1,65 @@
+package tacos.web;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.Ingredient;
+import tacos.Ingredient.Type;
+import tacos.Taco;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+	
+	@GetMapping
+	public String showDesignForm(Model model) {
+		List<Ingredient> ingredients = Arrays.asList(
+			new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+			new Ingredient("GRBF", "Ground Beaf", Type.PROTEIN),
+			new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+			new Ingredient("CHED", "Cheddar", Type.CHEESE),
+			new Ingredient("SLSA", "Salsa", Type.SAUCE)
+		);
+		
+		Type[] types = Ingredient.Type.values();
+		
+		for(Type type : types) {
+			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+		}
+		
+		model.addAttribute("taco", new Taco());
+		
+		return "design";
+		
+	}
+	
+	@PostMapping
+	public String porcessDesing(Taco design, Errors errors) {
+		
+		if(errors.hasErrors()) {
+			return "design";
+		}
+		
+		log.info("Processing design: " + design);
+		
+		return "redirect:/orders/current";
+	}
+
+	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+		return ingredients
+				.stream()
+				.filter(x -> x.getType().equals(type))
+				.collect(Collectors.toList());
+	}
+	
+}
